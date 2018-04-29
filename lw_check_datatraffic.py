83d03662dfb71afada075af387c1b5dc64bb7ef9
Milosz Galazka <milosz@sleeplessbeastie.eu>
2018-04-29 23:34:30 +0200

    Display LeaseWeb data traffic using API v2

--- lw_check_datatraffic.py
+++ lw_check_datatraffic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # use api keys to iterate over servers in each location
 # and display identifiable information and data traffic details
-# https://blog.sleeplessbeastie.eu/2017/11/20/how-to-display-leaseweb-data-traffic-using-command-line/
+# https://blog.sleeplessbeastie.eu/2018/04/30/how-to-display-leaseweb-data-traffic-using-api-v2/
 
 # import http.client
 import http.client
@@ -11,7 +11,25 @@ import json
 
 # import datetime
 import datetime
+import sys
 
+# math
+import math
+
+def pretty_size(nbytes, base=1000, decimal_part=2):
+  if base == 1000:
+    metric = ("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+  elif base == 1024:
+    metric = ("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
+  else:
+    return "%s %s" % (nbytes, "B")
+
+  if nbytes == 0:
+    return "%s %s" % ("0", "B")
+
+  nunit = int(math.floor(math.log(nbytes, base)))
+  nsize = round(nbytes/(math.pow(base, nunit)), decimal_part)
+  return '%s %s' % (nsize, metric[nunit])
 
 # current datetime
 now = datetime.datetime.now()
@@ -24,11 +42,11 @@ for key in api_keys:
     # get servers in location
     https_connection = http.client.HTTPSConnection("api.leaseweb.com")
     https_headers = { 'x-lsw-auth': api_keys[key] }
-    https_connection.request("GET", "/v1/bareMetals", headers=https_headers)
+    https_connection.request("GET", "/bareMetals/v2/servers", headers=https_headers)
     https_response = https_connection.getresponse()
     https_response_data = https_response.read()
     json_servers = json.loads(https_response_data.decode("utf-8"))
-    if not 'bareMetals' in json_servers:
+    if not 'servers' in json_servers:
       print('LeaseWeb', key, 'returned no data')
       continue
   except:
@@ -36,11 +54,16 @@ for key in api_keys:
     continue
 
   print('LeaseWeb', key)
-  for bareMetal in json_servers['bareMetals']:
+  for bareMetal in json_servers['servers']:
     try:
+      # get server info
+      https_connection.request('GET', '/bareMetals/v2/servers/' + bareMetal['id'], headers=https_headers)
+      https_response = https_connection.getresponse()
+      https_response_data = https_response.read()
+      json_server = json.loads(https_response_data.decode("utf-8"))
+
       # get datatraffic for server
-      # notice that you need to count datatraffic from the last day of previous month to the current day (to get the same results as found in customer portal)
-      https_connection.request('GET', '/v1/bareMetals/' + bareMetal['bareMetal']['bareMetalId'] + '/networkUsage/datatraffic?dateFrom=00-' + str(now.month) + '-' + str(now.year) + '&dateTo=' + str(now.day) + '-' + str(now.month) + '-' + str(now.year), headers=https_headers)
+      https_connection.request('GET', '/bareMetals/v2/servers/' + bareMetal['id'] + '/metrics/datatraffic?from=' + str(now.year) + '-' + str(now.month).rjust(2,'0') + '-01T00:00:00Z' + '&to=' + str(now.year) + '-' + str(now.month).rjust(2,'0') + '-' + str(now.day) + 'T00:00:00Z&aggregation=SUM', headers=https_headers)
       https_response = https_connection.getresponse()
       https_response_data = https_response.read()
       json_datatraffic = json.loads(https_response_data.decode("utf-8"))
@@ -50,7 +73,7 @@ for key in api_keys:
 
     try:
       # get IP addresses for server
-      https_connection.request('GET', '/v1/bareMetals/' + bareMetal['bareMetal']['bareMetalId'] + '/ips', headers=https_headers)
+      https_connection.request('GET', '/bareMetals/v2/servers/' + bareMetal['id'] + '/ips?networkType=public', headers=https_headers)
       https_response = https_connection.getresponse()
       https_response_data = https_response.read()
       json_ips= json.loads(https_response_data.decode("utf-8"))
@@ -61,28 +84,23 @@ for key in api_keys:
       continue
 
     # print results
-    print(' *', bareMetal['bareMetal']['serverName'])
-    print('   IP addresses: ', end='')
-    for index, json_ip in enumerate(json_ips["ips"]):
-      if index > 0: print(', ', end='')
-      print(json_ip['ip']['ip'], end='')
-    print()
-    print('   Interval from', json_datatraffic['dataTraffic']['interval']['from'], 'to', json_datatraffic['dataTraffic']['interval']['to'], end='')
-    if json_datatraffic['dataTraffic']['monthlyThreshold'] != "0 GB":
-      print(' with monthly threshold ', json_datatraffic['dataTraffic']['monthlyThreshold'], end='')
-    if json_datatraffic['dataTraffic']['overusage'] != "0 B":
-      print(' [', json_datatraffic['dataTraffic']['overusage'], '] ', end='')
-    print()
-    print('   Used ', json_datatraffic['dataTraffic']['measurement']['total'])
-    interface_index=1
-    for json_interface_datatraffic in json_datatraffic['dataTraffic']['measurement']:
-      try:
-        print('    - Interface ', str(interface_index), ': total', json_datatraffic['dataTraffic']['measurement'][json_interface_datatraffic]['total'], end=' = ')
-        print(json_datatraffic['dataTraffic']['measurement'][json_interface_datatraffic]['in'], 'in', end=' + ')
-        print(json_datatraffic['dataTraffic']['measurement'][json_interface_datatraffic]['out'], 'out')
-        if "node" in json_interface_datatraffic:
-          interface_index=interface_index+1
-      except:
-        continue
+    try:
+      print(' *', bareMetal['contract']['internalReference'])
+      print('   IP addresses: ', end='')
+      for index, json_ip in enumerate(json_ips["ips"]):
+        if index > 0: print(', ', end='')
+        print(json_ip['ip'], end='')
+      print()
+      if (str(json_server['contract']['networkTraffic']['datatrafficUnit']) != 'None'):
+        print('   Using network type', json_server['contract']['networkTraffic']['type'], 'traffic type', json_server['contract']['networkTraffic']['trafficType'], 'datatraffic limit', json_server['contract']['networkTraffic']['datatrafficLimit'], json_server['contract']['networkTraffic']['datatrafficUnit'], end='')
+        print()
+      print('   Interval from', json_datatraffic['_metadata']['from'], 'to', json_datatraffic['_metadata']['to'], end='\n')
+      print('   Data traffic on public interface: ', end='\n')
+      print('    ', pretty_size(json_datatraffic['metrics']['DOWN_PUBLIC']['values'][0]['value']), 'in', end=' ')
+      print(pretty_size(json_datatraffic['metrics']['UP_PUBLIC']['values'][0]['value']), 'out', end=' ')
+      print(pretty_size(int(json_datatraffic['metrics']['DOWN_PUBLIC']['values'][0]['value']) + int(json_datatraffic['metrics']['UP_PUBLIC']['values'][0]['value'])), 'total', end='')
+      print()
+    except:
+      continue
   https_connection.close()
 
